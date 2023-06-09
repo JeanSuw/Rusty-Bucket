5af3673070ebcec64aae7f5b7d7c1a62efc53d6a
moeba84 <119361569+moeba84@users.noreply.github.com>
2023-06-09 16:52:06 -0700

    changes to cards

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 
-import * as React from 'react';
+
+import React from 'react';
 //import { css } from '@emotion/css';
-//import {Card} from '../Card.js';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -19,148 +18,116 @@ import {Grid} from '@material-ui/core';
 
 export default function Home (){
   return(
-    <Grid container spacing ={4}>
-    <Grid item xs={12} sm={6} md={4}>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="254"
-          image={hotairballoon}
-          alt="Hot air balloon"
-        />
-        <CardContent>
-          <Typography variant="h5" fontWeight="bold" component="div">
-            Hot Air Balloon
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+    <Grid container spacing={4}>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card sx={{ maxWidth: 345, backgroundColor: 'transparent' }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="254"
+            image={hotairballoon}
+            alt="Hot air balloon" />
+          <CardContent>
+            <Typography variant="h5" fontWeight="bold" component="div">
+              Hot Air Balloon
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+            </Typography>
+          </CardContent>
+        </CardActionArea>
       </Card>
     </Grid>
     <Grid item xs={12} sm={6} md={4}>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="254"
-          image={northernlights}
-          alt="Hot air balloon"
-        />
-        <CardContent>
-          <Typography variant="h5" fontWeight="bold" component="div">
-             See Northern Lights
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      </Card>
-    </Grid>
-    <Grid item xs={12} sm= {6} md={4}>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="254"
-          image={Egypt}
-          alt="pyramid"
-        />
-        <CardContent>
-          <Typography variant="h5" fontWeight="bold" component="div">
-            Go to Egypt
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      </Card>
+        <Card sx={{ maxWidth: 345, backgroundColor: 'transparent'}}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="254"
+              image={northernlights}
+              alt="Hot air balloon" />
+            <CardContent>
+              <Typography variant="h5" fontWeight="bold" component="div">
+                See Northern Lights
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid><Grid item xs={12} sm={6} md={4}>
+        <Card sx={{ maxWidth: 345, backgroundColor: 'transparent' }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="254"
+              image={Egypt}
+              alt="pyramid" />
+            <CardContent>
+              <Typography variant="h5" fontWeight="bold" component="div">
+                Go to Egypt
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
       </Grid>
-      <Grid item xs={12} sm= {6} md={4}>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="254"
-          image={scubadiving}
-          alt="pyramid"
-        />
-        <CardContent>
-          <Typography variant="h5" fontWeight="bold" component="div">
-            Learn to Scuba Dive
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      </Card>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card sx={{ maxWidth: 345, backgroundColor: 'transparent' }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="254"
+              image={scubadiving}
+              alt="pyramid" />
+            <CardContent>
+              <Typography variant="h5" fontWeight="bold" component="div">
+                Learn to Scuba Dive
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
       </Grid>
-      <Grid item xs={12} sm= {6} md={4}>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="254"
-          image={skydiving}
-          alt="pyramid"
-        />
-        <CardContent>
-          <Typography variant="h5" fontWeight="bold" component="div">
-            Go Skydiving
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      </Card>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card sx={{ maxWidth: 345, backgroundColor: 'transparent' }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="254"
+              image={skydiving}
+              alt="pyramid" />
+            <CardContent>
+              <Typography variant="h5" fontWeight="bold" component="div">
+                Go Skydiving
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
       </Grid>
-      <Grid item xs={12} sm= {6} md={4}>
-    <Card sx = {{ maxWidth: 345 }} border = "rounded">
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="254"
-          image={marathon}
-          alt="marathon"
-        />
-        <CardContent>
-          <Typography variant="h5" fontWeight="bold" component="div">
-            Run a Marathon
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      </Card>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card sx={{ maxWidth: 345, backgroundColor: 'transparent' }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="254"
+              image={marathon}
+              alt="marathon" />
+            <CardContent>
+              <Typography variant="h5" fontWeight="bold" component="div">
+                Run a Marathon
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
       </Grid>
-    </Grid>
-
-   
-=======
-import React from 'react';
-
-const Home = () => {
-  return (
-    <div>
-      <h1>Coming Soon</h1>
-      <p>We are working on something exciting. Stay tuned!</p>
-    </div>
->>>>>>> c62400eabc9eb875bd0dda8b412b28fa19cd360f
+      </Grid>
   );
 }
-
-
-
-
-  
-  
       
-   
   
-
-
-
-
-
-
