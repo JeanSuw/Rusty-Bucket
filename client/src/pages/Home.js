88b85884bdeb27bf14713a8a8eea3a4f04674d25
moeba84 <119361569+moeba84@users.noreply.github.com>
2023-06-08 18:36:19 -0700

    added images and made changes to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -16,30 +16,36 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import hotairballoon from '../images/hot-air-balloon.jpg';
+import northernlights from '../images/northernlights.jpg';
+import pyramid from '../images/pyramid.jpg';
 
 export default function ActionAreaCard() {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
-          component="img"
+          component="img" 
           height="254"
-          image= "./public/hot-air-balloon.jpg"
-          alt="Hot Air Balloon"
+          image= {hotairballoon}
+          alt="hot air Balloon"
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
+          <Typography gutterBottom variant="h4" fontWeight="bold" component="div">
+            Hot Air Balloon
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
           </Typography>
         </CardContent>
       </CardActionArea>
     </Card>
+
+
   );
 }
+
+
+
 
 
 
